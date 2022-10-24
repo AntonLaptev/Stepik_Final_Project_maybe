013342cb66583ef7057f7ac9d3ca5fa6bf5d433e
AntonLaptev <aalaptev84@gmail.com>
2022-10-24 10:55:58 +0300

    добавлены файлы Page object, изменен тест

---
+++ pages/main_page.py
@@ -0,0 +1,5 @@
+from .base_page import BasePage
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element("css selector", "#login_link")
+        login_link.click()
