a71c69157ddf5be786b8a825c4b7eb6e10e7788a
AntonLaptev <aalaptev84@gmail.com>
2022-10-25 17:17:32 +0300

    добавлена Проверка элемента на странице

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,3 +3,5 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element("css selector", "#login_link")
         login_link.click()
+    def should_be_login_link(self):
+        assert self.browser.find_element("css selector","#login_link"), "Login link is not presented"
