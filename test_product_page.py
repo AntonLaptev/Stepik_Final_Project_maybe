645110e3a404972c007cd6770bd3a9b10da8efa3
AntonLaptev <aalaptev84@gmail.com>
2022-11-01 17:52:01 +0300

    переименован метод теста

--- test_product_page.py
+++ test_product_page.py
@@ -7,7 +7,38 @@ product_page_link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-w
 promo_product_page_link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
 
 
-@pytest.mark.need_review
+@pytest.mark.login_user
+class TestUserAddToBasketFromProductPage:
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = 'qweq345uefhue'
+        page = ProductPage(browser, product_page_link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.register_new_user(email, password)
+        login_page.should_be_success_registration_message()
+        login_page.should_be_authorized_user()
+
+    def test_user_cant_see_success_message(self, browser):
+        page = ProductPage(browser, product_page_link)
+        page.open()
+        page.should_not_see_success_message()
+
+    @pytest.mark.need_review
+    def test_user_can_add_product_to_basket(self, browser):
+        page = ProductPage(browser, promo_product_page_link)
+        page.open()
+        page.should_be_data_on_product_page()
+        page.get_product_price_and_title_from_product_page()
+        page.put_product_to_the_basket()
+        page.solve_quiz_and_get_code()
+        page.should_be_product_in_basket()
+
+
+@pytest.mark.need_review  # параметризация убрана специально, чтобы не увеличивать тест.
 def test_guest_can_add_product_to_basket(browser):
     page = ProductPage(browser, promo_product_page_link)  # инициализируем Page Object, передаем   драйвер и url
     page.open()  # открываем страницу
@@ -63,34 +94,3 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_not_be_products_in_basket()
     basket_page.should_be_empty_basket_message()
-
-
-@pytest.mark.login_user
-class TestUserAddToBasketFromProductPage:
-
-    @pytest.fixture(scope="function", autouse=True)
-    def setup(self, browser):
-        email = str(time.time()) + "@fakemail.org"
-        password = 'qweq345uefhue'
-        page = ProductPage(browser, product_page_link)
-        page.open()
-        page.go_to_login_page()
-        login_page = LoginPage(browser, browser.current_url)
-        login_page.register_new_user(email, password)
-        login_page.should_be_success_registration_message()
-        login_page.should_be_authorized_user()
-
-    def test_guest_cant_see_success_message(self, browser):
-        page = ProductPage(browser, product_page_link)
-        page.open()
-        page.should_not_see_success_message()
-
-    @pytest.mark.need_review
-    def test_user_can_add_product_to_basket(self, browser):
-        page = ProductPage(browser, promo_product_page_link)
-        page.open()
-        page.should_be_data_on_product_page()
-        page.get_product_price_and_title_from_product_page()
-        page.put_product_to_the_basket()
-        page.solve_quiz_and_get_code()
-        page.should_be_product_in_basket()
